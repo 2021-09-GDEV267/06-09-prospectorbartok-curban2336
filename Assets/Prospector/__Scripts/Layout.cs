7ea9dd20b46cb92d34a2f7a85812333f59ac15e5
curban2336 <curban841@gmail.com>
2021-10-17 18:03:23 -0400

    Small syntax fixes

--- Assets/Prospector/__Scripts/Layout.cs
+++ Assets/Prospector/__Scripts/Layout.cs
@@ -79,7 +79,7 @@ public class Layout : MonoBehaviour
                     drawPile = tSD;
                     break;
 
-                case "discardPile":
+                case "discardpile":
                     discardPile = tSD;
                     break;
             }
